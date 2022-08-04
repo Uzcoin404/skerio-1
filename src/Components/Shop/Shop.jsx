aa17a5158464d47c052024effee814f3897d2020
SayKham99 <x5292621@gmail.com>
2022-08-04 19:52:26 +0500

    resolve

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -181,6 +181,7 @@ function Shop({ cart }) {
 const mapStateToProps = (state) => {
     return {
         cart: state.shop.cart,
+        cart: state.shop.cart,
     };
 };
 export default connect(mapStateToProps)(Shop);
